f5e71cc00454292b34d19fe595a8c29afe478acf
George J <georgejambooken@gmail.com>
2023-03-05 09:42:09 +0530

    added the random button
    it doesn't do anything, just turns on and off.
    also added codeword buttons on the recevier side

---
+++ Hamming Code/Assets/Scripts/Randomizer.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Randomizer : MonoBehaviour
+{
+    bool stats=false;
+    public GameObject r1txt;
+    public void activate()
+    {
+        if(stats==false)
+        {
+            stats=true;
+            randomize();
+        }
+        else
+        {
+            stats=false;
+            r1txt.GetComponentInChildren<Text>().text = "Random Error : OFF";
+        }
+         
+    }
+    public void randomize()
+    {
+        r1txt.GetComponentInChildren<Text>().text = "Random Error : ON";
+    }
+}
