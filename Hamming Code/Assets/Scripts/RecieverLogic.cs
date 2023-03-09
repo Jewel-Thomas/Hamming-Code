e0be21817706b59d5546fda7120ae7f78a23082d
Jewel-Thomas <jewelthomas20@gmail.com>
2023-03-09 21:41:42 +0530

    dfgdfg

--- Hamming Code/Assets/Scripts/RecieverLogic.cs
+++ Hamming Code/Assets/Scripts/RecieverLogic.cs
@@ -8,6 +8,8 @@ public class RecieverLogic : MonoBehaviour
     public Button[] button;
     public buttonstuff[] bs;
     public BitManager bitManager;
+    public GameObject[] synGameobject;
+    public int s1,s2,s0;
 
     // Start is called before the first frame update
     void Start()
@@ -26,6 +28,7 @@ public class RecieverLogic : MonoBehaviour
         {
             CopySenderCodeWord();
         }
+        CalculateSyndrome();
     }
 
     public void ErrorMade()
@@ -43,12 +46,14 @@ public class RecieverLogic : MonoBehaviour
             button[rand].GetComponentInChildren<Text>().text="1";
             button[rand].GetComponent<Image>().color = Color.red;
             button[rand].GetComponentInChildren<Text>().color= Color.white;
+            bs[rand].bit = 1;
         }
         else
         {
             button[rand].GetComponentInChildren<Text>().text="0";
             button[rand].GetComponent<Image>().color = Color.white;
             button[rand].GetComponentInChildren<Text>().color= Color.black;
+            bs[rand].bit = 0;
         }
         DisableAll();
     }
@@ -56,17 +61,7 @@ public class RecieverLogic : MonoBehaviour
     {
         for(int i=0;i<7;i++)
         {
-            if(bitManager.bitArray[i]!=bs[i].bit)
-            {
-                for(int j=0;j<i;j++)
-                {
-                    button[j].interactable = false;
-                }
-                for(int j=i+1;j<7;j++)
-                {
-                    button[j].interactable = false;
-                }
-            }
+            button[i].interactable = false;
         }
     }
 
@@ -79,6 +74,46 @@ public class RecieverLogic : MonoBehaviour
         Randomizer.stats = false;
     }
 
+    void CalculateSyndrome()
+    {
+        s2 = (bs[2].bit + bs[3].bit + bs[0].bit + bs[4].bit)%2;
+        s1 = (bs[0].bit + bs[1].bit + bs[2].bit + bs[5].bit)%2;
+        s0 = (bs[1].bit + bs[2].bit + bs[3].bit + bs[6].bit)%2; 
+        synGameobject[0].GetComponentInChildren<Text>().text = s2.ToString();
+        if(s2==1)
+        {
+            synGameobject[0].GetComponent<Image>().color = Color.red;
+            synGameobject[0].GetComponentInChildren<Text>().color = Color.white;
+        }
+        else
+        {
+            synGameobject[0].GetComponent<Image>().color = Color.white;
+            synGameobject[0].GetComponentInChildren<Text>().color = Color.black;
+        }
+        synGameobject[1].GetComponentInChildren<Text>().text = s1.ToString();
+        if(s1==1)
+        {
+            synGameobject[1].GetComponent<Image>().color = Color.red;
+            synGameobject[1].GetComponentInChildren<Text>().color = Color.white;
+        }
+        else
+        {
+            synGameobject[1].GetComponent<Image>().color = Color.white;
+            synGameobject[1].GetComponentInChildren<Text>().color = Color.black;
+        }
+        synGameobject[2].GetComponentInChildren<Text>().text = s0.ToString();
+        if(s0==1)
+        {
+            synGameobject[2].GetComponent<Image>().color = Color.red;
+            synGameobject[2].GetComponentInChildren<Text>().color = Color.white;
+        }
+        else
+        {
+            synGameobject[2].GetComponent<Image>().color = Color.white;
+            synGameobject[2].GetComponentInChildren<Text>().color = Color.black;
+        }
+    }
+
     public void CopySenderCodeWord()
     {
         for(int i=0;i<7;i++)
@@ -86,13 +121,13 @@ public class RecieverLogic : MonoBehaviour
             button[i].GetComponentInChildren<Text>().text = bitManager.bitArray[i].ToString();
             if(bitManager.bitArray[i]==1)
             {
-                // bs[i].bit = bitManager.bitArray[i];
+                bs[i].bit = bitManager.bitArray[i];
                 button[i].GetComponent<Image>().color = Color.red;
                 button[i].GetComponentInChildren<Text>().color= Color.white;
             }
             else
             {
-                // bs[i].bit = bitManager.bitArray[i];
+                bs[i].bit = bitManager.bitArray[i];
                 button[i].GetComponent<Image>().color = Color.white;
                 button[i].GetComponentInChildren<Text>().color= Color.black;
             }
